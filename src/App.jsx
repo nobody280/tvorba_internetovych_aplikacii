24e4beca657237816b3be4a9ee886f40b98978d6
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-03-23 22:49:25 +0100

    Basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,31 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const today = new Date();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const items = Array(daysInMonth).fill(null);
 
   return (
     <>
-      HELLO
+
+      <nav>
+        <button type='button' onClick="alert('Click')">New Task</button>
+        <button type='button' onClick="alert('Click')">New Project</button>
+      </ nav>
+
+      <div class="calendar">
+        <div class = "month"><button type='button' onClick="">left</button><h2>{today.toLocaleString("default", { month: "long" })} {year}</h2><button type='button' onClick="">right</button></div>
+        <div class = "monthbox">
+          {items.map((_, index) => (
+            <div class="daybox">
+              {index + 1}.
+            </div>
+          ))}
+        </div>
+        
+      </div>
     </>
   )
 }
