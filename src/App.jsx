eed45f3ae5eb5af524f820da2bfaae8222f14cd3
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-03-24 00:04:01 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [taskWindow, setShowTask] = useState(false);
   const [count, setCount] = useState(0)
   const today = new Date();
   const month = today.getMonth();
@@ -11,16 +13,34 @@ function App() {
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const items = Array(daysInMonth).fill(null);
 
+  const addTask = () => {
+    setShowTask(!taskWindow);
+    console.log("ADD TASK"); 
+  }; 
+
   return (
     <>
 
       <nav>
-        <button type='button' onClick="alert('Click')">New Task</button>
+        <button type='button' onClick={addTask}>New Task</button>
         <button type='button' onClick="alert('Click')">New Project</button>
       </ nav>
 
+      {taskWindow && (
+        <div class = "taskWindow">
+          <h3>Task Info:</h3>
+          <div class = "form">
+            <input type="text" id="name" name="name" placeholder="Task name" />
+            <input type="text" id="discription" name="discription" placeholder="Discription" />
+            
+          </div>
+          
+          
+        </div>
+      )}
+
       <div class="calendar">
-        <div class = "month"><button type='button' onClick="">left</button><h2>{today.toLocaleString("default", { month: "long" })} {year}</h2><button type='button' onClick="">right</button></div>
+        <div class = "month"><button type='button' onClick="">&laquo;</button><h2>{today.toLocaleString("default", { month: "long" })} {year}</h2><button type='button' onClick="">&raquo;</button></div>
         <div class = "monthbox">
           {items.map((_, index) => (
             <div class="daybox">
