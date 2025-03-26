99d4a6cf9f768d116a1192a15795dfc08eb84ba0
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-03-26 10:54:37 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,14 +1,6 @@
 import pg from 'pg';
 const { Client } = pg;
 
-const client = new Client({
-    host: 'localhost', 
-    user: 'postgres', 
-    password: 'mfd8amb8fu',
-    database: 'my_app', 
-    port: 5432 
-});
-
 client.connect()
     .then(() => console.log('Connected to the PostgreSQL database'))
     .catch(err => console.error('Database connection failed:', err.stack));
