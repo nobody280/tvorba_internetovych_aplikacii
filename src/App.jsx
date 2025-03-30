92097c8c0dc7a311a256c04d1765fef0eace52db
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-03-30 20:52:38 +0200

    Update button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,45 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([]);
   const [taskWindow, setShowTask] = useState(false);
   const [count, setCount] = useState(0)
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [year, setYear] = useState(new Date().getFullYear()); 
   const today = new Date();
-  const month = today.getMonth();
-  const year = today.getFullYear();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const items = Array(daysInMonth).fill(null);
+
+  
 
   const addTask = () => {
     setShowTask(!taskWindow);
     console.log("ADD TASK"); 
   }; 
 
+  const oneMore = () => {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const oneLess = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1); 
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
   return (
     <>
-
       <nav>
         <button type='button' onClick={addTask}>New Task</button>
-        <button type='button' onClick="alert('Click')">New Project</button>
+        <button type='button' onClick={addTask}>New Project</button>
       </ nav>
 
       {taskWindow && (
@@ -40,7 +60,10 @@ function App() {
       )}
 
       <div class="calendar">
-        <div class = "month"><button type='button' onClick="">&laquo;</button><h2>{today.toLocaleString("default", { month: "long" })} {year}</h2><button type='button' onClick="">&raquo;</button></div>
+        <div class = "month">
+          <button type='button' onClick={oneLess}>&laquo;</button><h2>
+            {new Date(year, month).toLocaleString("default", { month: "long" })} {year}</h2>
+          <button type='button' onClick={oneMore}>&raquo;</button></div>
         <div class = "monthbox">
           {items.map((_, index) => (
             <div class="daybox">
