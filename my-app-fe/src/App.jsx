5cf09cac2cad415d5b938d335eb75d238abc89c3
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-04-06 21:12:07 +0200

    Separate frontend and backend files

---
+++ my-app-fe/src/App.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [taskWindow, setShowTask] = useState(false);
+  const [count, setCount] = useState(0)
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [year, setYear] = useState(new Date().getFullYear()); 
+  const today = new Date();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const items = Array(daysInMonth).fill(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000') 
+      .then(response => response.json()) 
+      .then(data => {
+        setData(data);
+        setLoading(false); 
+      })
+      .catch(err => {
+        setError(err); 
+        setLoading(false); 
+  }, []); 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>; 
+  }
+  
+
+  const addTask = () => {
+    setShowTask(!taskWindow);
+  };
+  
+  const addProject = () => {
+    setShowTask(!taskWindow);
+  };
+
+  const oneMore = () => {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const oneLess = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1); 
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  return (
+    <>
+      <nav>
+        <button type='button' onClick={addTask}>New Task</button>
+        <button type='button' onClick={addProject}>New Project</button>
+      </ nav>
+
+      {taskWindow && (
+        <div className = "taskWindow">
+          <h3>Task Info:</h3>
+          <div class = "form">
+            <input type="text" id="name" name="name" placeholder="Task name" />
+            <input type="text" id="discription" name="discription" placeholder="Discription" />
+            
+          </div>
+          
+          
+        </div>
+      )}
+
+      <div className="calendar">
+        <div className = "month">
+          <button type='button' onClick={oneLess}>&laquo;</button><h2>
+            {new Date(year, month).toLocaleString("default", { month: "long" })} {year}</h2>
+          <button type='button' onClick={oneMore}>&raquo;</button></div>
+        <div className = "monthbox">
+          {items.map((_, index) => (
+            <div className="daybox">
+              {index + 1}.
+            </div>
+          ))}
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
