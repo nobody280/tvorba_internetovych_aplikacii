f29f2d930892a9f46bd3f2bb6db074ba35f38e3b
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-04-14 20:04:55 +0200

    Added login

--- my-app-be/routes/users.js
+++ my-app-be/routes/users.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import client from '../db.js';
 
 const router = express.Router();
@@ -10,6 +11,38 @@ router.get('/', async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/', async (req, res)  => {
+  console.log('POST request received');
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Missing username or password' });
+  }
+
+  try {
+    const names = username.split(' ');
+    const result = await client.query('SELECT * FROM accounts JOIN users ON accounts.user_id=users.id WHERE users.first_name = $1 AND users.last_name = $2', names);
+    const user = result.rows[0];
+
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username ' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.status(200).json({ message: 'Login successful', userId: user.id });
+
+  } catch (err) {
+    console.error('Error during login:', err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
--- my-app-fe/src/App.jsx
+++ my-app-fe/src/App.jsx
@@ -6,8 +6,11 @@ import './App.css'
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [logged, setIsLogged] = useState(false);
   const [error, setError] = useState(null);
   const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [taskWindow, setShowTask] = useState(false);
   const [count, setCount] = useState(0);
   const [month, setMonth] = useState(new Date().getMonth());
@@ -62,42 +65,78 @@ function App() {
     return <div>Error: {error.message}</div>;
   }
 
-  return (
-    <>
-      <nav>
-      {users.map(user => (
-          <div>{user.first_name} {user.last_name}</div>
-        ))}
-        <button type="button" onClick={addTask}>New Task</button>
-        <button type="button" onClick={addProject}>New Project</button>
-      </nav>
+  if (logged) {
+    return (
+      <>
+        <nav>
+        {users.map(user => (
+            <div>{user.first_name} {user.last_name}</div>
+          ))}
+          <button type="button" onClick={addTask}>New Task</button>
+          <button type="button" onClick={addProject}>New Project</button>
+        </nav>
 
-      {taskWindow && (
-        <div className="taskWindow">
-          <h3>Task Info:</h3>
-          <div className="form">
-            <input type="text" id="name" name="name" placeholder="Task name" />
-            <input type="text" id="description" name="description" placeholder="Description" />
+        {taskWindow && (
+          <div className="taskWindow">
+            <h3>Task Info:</h3>
+            <div className="form">
+              <input type="text" id="name" name="name" placeholder="Task name" />
+              <input type="text" id="description" name="description" placeholder="Description" />
+            </div>
+          </div>
+        )}
+
+        <div className="calendar">
+          <div className="month">
+            <button type="button" onClick={oneLess}>&laquo;</button>
+            <h2>{new Date(year, month).toLocaleString("default", { month: "long" })} {year}</h2>
+            <button type="button" onClick={oneMore}>&raquo;</button>
+          </div>
+          <div className="monthbox">
+            {items.map((_, index) => (
+              <div className="daybox" key={index}>
+                {index + 1}.
+              </div>
+            ))}
           </div>
         </div>
-      )}
-
-      <div className="calendar">
-        <div className="month">
-          <button type="button" onClick={oneLess}>&laquo;</button>
-          <h2>{new Date(year, month).toLocaleString("default", { month: "long" })} {year}</h2>
-          <button type="button" onClick={oneMore}>&raquo;</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+      <div className = "login">
+        <div className='month'>
+          <h2>Loggin</h2>
         </div>
-        <div className="monthbox">
-          {items.map((_, index) => (
-            <div className="daybox" key={index}>
-              {index + 1}.
-            </div>
-          ))}
-        </div>
+        
+        <form>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" value={username} onChange={e => setUsername(e.target.value)} ></input>
+          <br></br>
+          <label htmlFor="password">Last name:</label>
+          <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} ></input>
+          <br></br>
+          <button 
+            onClick={(e) => {
+              e.preventDefault();
+              axios.post('/api/users', { username, password })
+              .then(response => {
+                setIsLogged(true);
+              })
+              .catch(error => {
+                console.error('Login failed:', error);
+              });
+            }}
+          >
+            Login
+          </button>
+        </form> 
       </div>
-    </>
-  );
+      </>
+    );
+  }
+  
 }
 
 export default App;
