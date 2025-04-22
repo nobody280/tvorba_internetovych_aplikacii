1e1012c3871f4e2bfd97aef87a61f0363ff093ad
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-04-23 01:27:01 +0200

    Update for render

--- my-app-be/db.js
+++ my-app-be/db.js
@@ -1,4 +1,12 @@
-import client from './config.secrets.js'; 
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,  
+  port: 5432,  
+  database: process.env.DB_DATABASE,
+});
 
 client.connect()
   .then(() => console.log('Connected to PostgreSQL database'))
