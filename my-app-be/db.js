271d7c0095aa16c1e1cb908604c5ae8677fd6fb8
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-05-05 15:14:07 +0200

    Try to fix isue with render (take 2)

--- my-app-be/db.js
+++ my-app-be/db.js
@@ -5,7 +5,11 @@ const { Client } = pg;
 dotenv.config();
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false,
   },
