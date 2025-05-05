a40252b79deb543e8350003ecd5544aba2f4c744
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-05-05 14:34:25 +0200

    Update due to render problems
    render.com couldn't connect to database correctly due to error in code.

--- my-app-be/db.js
+++ my-app-be/db.js
@@ -4,17 +4,8 @@ import dotenv from 'dotenv';
 const { Client } = pg;
 dotenv.config();
 
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-
 const client = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,  
-  port: 5432,  
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: false,
 });
 
