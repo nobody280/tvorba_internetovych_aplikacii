f90bf871ab7b810a2c584c34e8de25e463c9194b
jaremcukova <119945537+nobody280@users.noreply.github.com>
2025-05-06 14:24:36 +0200

    Reupload dist after changes in vite.config

--- my-app-fe/vite.config.js
+++ my-app-fe/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', 
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
-      },
     },
+  },
   plugins: [react()],
 })
